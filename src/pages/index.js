48b80627eb432a179dcd0fd7bb6d52ee3af49de0
Mauricio Edson de Souza <mauricioesdonsouza@gmail.com>
2022-04-04 15:40:58 -0300

    Estrutura inicial do react com next js

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.scss'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Home</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1>Titulo</h1>
+      </main>
+    </div>
+  )
+}
